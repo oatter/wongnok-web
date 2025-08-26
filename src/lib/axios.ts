5e2269e6b36d79c1d4721cec078806d763159f92
Naravit Bunthap <naravit@odds.team>
2025-08-26 22:11:56 +0700

    - update get session in server side from next auth

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import { getSession } from 'next-auth/react'
+import { getServerSession } from 'next-auth'
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_PATH,
 })
 
 api.interceptors.request.use(async (config) => {
-  const session = await getSession()
+  const session = await getServerSession()
   config.headers.Authorization = `Bearer ${session?.accessToken || ''}`
   return config
 })
